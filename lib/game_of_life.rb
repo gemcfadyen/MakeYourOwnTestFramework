0f24d054d25d0b95840eb28af3f052039bdd6aea
gemcfadyen <gemcfadyen@hotmail.co.uk>
2016-02-07 20:23:32 +0000

    Improved error messages for draw test

--- lib/game_of_life.rb
+++ lib/game_of_life.rb
@@ -18,7 +18,7 @@ class Player
   attr_reader :name, :moves
 end
 
-def assert_winner(winner, looser=nil, test_name)
+def assert_winner(winner, looser, test_name)
   if !winner.winner?
     puts "!!!!!!! Expected #{winner.name} to win the game"
   end
@@ -31,6 +31,41 @@ def assert_winner(winner, looser=nil, test_name)
     puts "--------------------------"
   end
 end
+
+
+def assert_draw(winner, looser, test_name)
+  if winner.winner?
+    puts "!!!!!!! Expected a draw but #{winner.name} won the game"
+  end
+  if looser.winner?
+    puts "!!!!!!! Expected a draw but #{looser.name} won the game"
+  end
+
+  if !winner.winner? && !looser.winner?
+    puts "PASSED #{test_name}"
+    puts "--------------------------"
+  end
+end
+
+
+
+
+draw = lambda { |name|
+  jorgeena = Player.new("Jorgeena")
+  pawelek = Player.new("Pawelek")
+
+  jorgeena.take(1)
+  pawelek.take(2)
+  jorgeena.take(3)
+  pawelek.take(6)
+  jorgeena.take(4)
+  pawelek.take(7)
+  jorgeena.take(5);
+  pawelek.take(9)
+  jorgeena.take(8);
+
+ assert_draw(jorgeena, pawelek, name)
+}
 
 jorgeena_wins = lambda { |name|
   jorgeena = Player.new("Jorgeena")
@@ -59,9 +94,10 @@ pawelek_wins = lambda { |name|
 }
 
 
-def run_tests(name, test_one)
+def run_test(name, test_one)
   test_one.call name
 end
 
-run_tests("pawelek_wins", pawelek_wins)
-run_tests("jorgeena_wins", jorgeena_wins)
+run_test("pawelek_wins", pawelek_wins)
+run_test("jorgeena_wins", jorgeena_wins)
+run_test("draw", draw)
